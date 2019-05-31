677d859bfccba0ad4f9fd993b4d89466556a258a
Lindokuhle Momoza <lindokuhle.momoza@umuzi.org>
2019-05-31 16:33:54 +0200

    first commit

--- src/calculator.js
+++ src/calculator.js
@@ -1,18 +1,40 @@
-//add numbers
-function addNum(num1, num2, num3, num4){
-    var sum = 0;
-for(let i = 0; i<arguments.length; i++){
-    sum += arguments[i];
-}
-    return sum;
-}
+class Calculator{
+    constructor(){
+        this.lastNum = 0;
+        this.slot = [];
+    }
+    //add numbers
+    addNum(){
+        var sum = 0;
+        for(let i = 0; i<arguments.length; i++){
+            sum += arguments[i];
+        }
+        this.lastNum = sum;
+        return sum;
+    }
 
 
-//muiltiply numbers
-function multiplyNum(num1, num2, num3, num4){
-    var sum = 1;
-for(let i = 0; i<arguments.length; i++){
-    sum *= arguments[i];
-}
-    return sum;
-}
+    //muiltiply numbers
+    multiplyNum(){
+        var product = 1;
+        for(let i = 0; i<arguments.length; i++){
+            product *= arguments[i];
+        }
+        this.lastNum = product;
+        return product;
+    }
+
+    //return last
+    last(){
+        return this.lastNum;
+    }
+    
+    set_slot(number){
+        this.slot[number] = this.lastNum;
+    }
+    
+    get_slot(number){
+        return this.slot[number];
+    }
+};
+
